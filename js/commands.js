8e4bc590ad6593ce69985fe0fd94e2806c95972d
MaxVdp <max.vandeput@student.ucll.be>
2024-05-05 19:00:59 +0200

    full re-design - terminal

---
+++ js/commands.js
@@ -0,0 +1,58 @@
+var startMsg = [
+    
+` 
+    __  ___                _    __                   __                     __ 
+   /  |/  /____ _ _  __   | |  / /____ _ ____   ____/ /___   ____   __  __ / /_
+  / /|_/ // __ \`/| |/_/   | | / // __ \`// __ \\ / __  // _ \\ / __ \\ / / / // __/
+ / /  / // /_/ /_>  <     | |/ // /_/ // / / // /_/ //  __// /_/ // /_/ // /_  
+/_/  /_/ \\__,_//_/|_|     |___/ \\__,_//_/ /_/ \\__,_/ \\___// .___/ \\__,_/ \\__/  
+                                                         /_/          © 2024    
+
+
+<p>Welcome to my terminal portfolio.</p>
+<p>For a list of available commands, type '<span>help</span>'.</p><br></br>
+`
+]
+
+var help = [
+    '<span class="outName">help</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Obviously, you know what this does',
+    '<span class="outName">whois</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Who is Max Vandeput?',
+    '<span class="outName">projects</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;View Max\'s projects',
+    '<span class="outName">socials</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;View Max\'s socials',
+    '<span class="outName">history</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;View command history',
+    '<span class="outName">clear</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Clear the terminal',
+    '<span class="outName">exit</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Exit the terminal',
+    '<span class="outName">ls</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;View files in current directory<br></br>',
+]
+
+var whois = [
+    "Hello I am Max Vandeput.<br></br>",
+    "I am currently studying for a Bachelor's degree in Applied Informatics at UCLL.",
+    "As a software developer, I am passionate about building high-quality software that solves user needs.",
+    "I am always looking for ways to expand and improve my skills and knowledge",
+    "so that I can continue to do my job in the best possible way.<br></br>",
+    "I have experience with a variety of programming languages and technologies,",
+    "including Java, Python, JavaScript, HTML, CSS, Gleam and SQL.",
+    "I have also used frameworks and libraries such as React, Node.js, and Express for school projects.<br></br>", 
+    "When I am not studying or working on personal projects, you can find me doing calisthenics, bouldering,","playing the guitar, or running around the table with my dog.<br></br>",
+
+]
+
+
+var socials = [
+    '<span class="outName">LinkedIn</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.linkedin.com/in/max-vandeput/" target="_blank">linkedin.com/in/max-vandeput/</a>',
+
+    '<span class="outName">GitHub</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/MaxVdp" target="_blank">github.com/MaxVdp</a>',
+
+    '<span class="outName">cv </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="files/MaxVdp_Nederlands_cv.pdf" target="_blank">Dutch-cv/MaxVdp</a><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="files/MaxVdp_English_cv.pdf" target="_blank">English-cv/MaxVdp</a><br></br>',
+
+]
+    
+
+var projects = [
+    "Projects are currently being worked on and will be available soon.<br></br>",
+]
+
+var notFound = [
+    "Command not found. Type 'help' for a list of available commands.<br></br>",
+]
---
+++ js/main.js
@@ -0,0 +1,161 @@
+let history = [];
+var historyIndex = -1;
+
+window.onload = function() {
+    var terminal = document.getElementById('terminal');
+    var div = document.createElement('div');
+    div.className = 'outputSection';
+    var lines = startMsg[0].split('\n'); // Split the input into lines
+    for(var i = 0; i < lines.length; i++){
+        (function(index) {
+            setTimeout(function(){
+                var pre = document.createElement("pre");
+                pre.innerHTML = lines[index]; // Print each line separately
+                div.appendChild(pre);
+                terminal.scrollTop = terminal.scrollHeight; // Scroll to the bottom
+            }, index * 100);
+        })(i);
+    }
+    terminal.appendChild(div);
+}
+
+document.addEventListener('keydown', function(event) {
+    var textarea = document.getElementById('input');
+    if (document.activeElement !== textarea) {
+        textarea.focus();
+    }
+});
+
+
+var terminal = document.getElementById('terminal')
+var input = document.getElementById('input');
+var commands = ['ls', 'exit', 'clear', 'help', 'whois', 'projects', 'socials', 'history']; 
+
+// Run when clicked on the input line 
+function startTyping() {
+    var textarea = document.getElementById('input');
+    textarea.focus();
+}
+
+
+function typeOut(textarea, event) {
+    var writter = document.getElementById('writter');
+    writter.textContent = textarea.value;
+
+}
+
+function handleKeydown(textarea, event) {
+    switch(event.keyCode){
+        case 13: // Enter key
+            var trimmedValue = textarea.value.trim();
+            if (trimmedValue !== '') {
+                command(trimmedValue);
+                history.push(trimmedValue);
+                historyIndex = -1; // Reset the history index
+            }
+            textarea.value = '';
+            break;
+        case 38: // Up arrow key
+            event.preventDefault(); // Prevent default scrolling behavior
+            if (history.length > 0 && historyIndex < history.length - 1) {
+                historyIndex++;
+                textarea.value = history[history.length - 1 - historyIndex];
+            }
+            break;
+        case 40: // Down arrow key
+            event.preventDefault(); // Prevent default scrolling behavior
+            if (historyIndex > 0) {
+                historyIndex--;
+                textarea.value = history[history.length - 1 - historyIndex];
+            } else if (historyIndex === 0) {
+                historyIndex--;
+                textarea.value = '';
+            }
+            break;
+        case 9:
+            event.preventDefault();
+            var inputValue = input.value.trim();
+            for (var i = 0; i < commands.length; i++) {
+                if (commands[i].startsWith(inputValue)) {
+                    input.value = commands[i];
+                    break;
+                }
+            }
+    }
+    typeOut(textarea);
+}
+
+
+
+function command(cmd){
+    switch(cmd.toLowerCase()){
+        case 'help':
+            printLine("help");
+            printOutput(help);
+            break;
+        case 'whois':
+            printLine("whois");
+            printOutput(whois);
+            break;
+        case 'projects':
+            printLine("projects");
+            printOutput(projects);
+            break;
+        case 'socials':
+            printLine("socials");
+            printOutput(socials);
+            break;
+        case 'history':
+            printLine("history");
+            printOutput(history);
+            break;
+        case 'clear':
+            clear();
+            break;
+        case 'exit':
+            break;
+        //Optional command
+        // case 'ls':
+        //     break;
+        default:
+            printLine(cmd.toLowerCase());
+            printOutput(notFound);
+            break;
+    }
+}
+
+function printLine(cmd){
+    var terminal = document.getElementById('terminal');
+    var div = document.createElement('div');
+    var p = document.createElement('p');
+    p.textContent = 'guest@MX-Shell:~$ ';
+    var span = document.createElement('span');
+    span.textContent = cmd;
+    p.appendChild(span);
+    div.appendChild(p);
+    terminal.appendChild(div);
+    window.scrollTo(0,document.body.scrollHeight);
+}
+
+
+function printOutput(cmd){
+    var terminal = document.getElementById('terminal');
+    var div = document.createElement('div');
+    div.className = 'outputSection';
+    for(var i = 0; i < cmd.length; i++){
+        (function(index) {
+            setTimeout(function(){
+                var p = document.createElement("p");
+                p.innerHTML = cmd[index];
+                div.appendChild(p);
+                window.scrollTo(0,document.body.scrollHeight);
+            }, index * 100);
+        })(i);
+    }
+    terminal.appendChild(div);
+}
+
+function clear(){
+    var terminal = document.getElementById('terminal');
+    terminal.innerHTML = '';
+}
